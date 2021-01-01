9a78a4b841f53c67b44440a7961b5192f82db869
Lucaskyy <lucabrio2005@gmail.com>
2021-01-01 21:16:13 +0100

    add isGameRunning

--- src/main/java/xyz/sculas/fateapi/scenario/listener/ScenarioListener.java
+++ src/main/java/xyz/sculas/fateapi/scenario/listener/ScenarioListener.java
@@ -1,10 +1,13 @@
 package xyz.sculas.fateapi.scenario.listener;
 
 import net.fateuhc.plugin.scenarios.Scenario;
+import net.fateuhc.plugin.states.GameState;
 import org.bukkit.event.Listener;
 import xyz.sculas.fateapi.scenario.ScenarioManager;
 
 public class ScenarioListener implements Listener {
+
+    private final ScenarioManager scenarioManager;
 
     /**
      * Register a listener for a scenario
@@ -21,8 +24,13 @@ public class ScenarioListener implements Listener {
      * @param scenario the scenario this listener is mapped to
      */
     public ScenarioListener(ScenarioManager scenarioManager, Scenario scenario) {
+        this.scenarioManager = scenarioManager;
         if(scenario == null) throw new IllegalArgumentException("That scenario does not exist");
         scenarioManager.register(scenario, this);
+    }
+
+    public boolean isGameRunning() {
+        return scenarioManager.raw().getPlugin().getGameManager().getGameState() != GameState.values()[0];
     }
 
 }
